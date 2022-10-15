b3ffbb488692ed1686cb9eb98ba63533a4285976
Olumide Bambe <loyalsfc@gmail.com>
2022-10-15 08:16:42 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="user_data.php" method="post">
+    Name: <input type="text" name="name"><br>
+    E-mail: <input type="text" name="email"><br>
+    DOB: <input type="date" name="date"><br>
+    Country: <input type="text" name="country"><br>
+    <input type="submit">
+</form>
+
+</body>
+</html>
